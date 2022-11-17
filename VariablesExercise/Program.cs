7d707ff7f25da6d858adbc871e20ca065259c04c
Michael <m.zaluckyj@gmail.com>
2022-11-17 15:22:32 -0500

    completed exercises

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace VariablesExercise
-{
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//declare and Initialized
+
+using System;
+
+string name = "Mike";
+int age = 30;
+char middleInital = 'R';
+bool isOver18 = true;
+double currentTemp = 32;
+decimal currentPrice = 19.99m;
+
+//writing the variables
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Inital: {middleInital}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
